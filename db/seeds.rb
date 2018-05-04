bcc7adb57be55d89cad2fbe73da61be63b42d25e
orihara <orihara.hosei@gmail.com>
2018-05-04 16:59:54 +0900

    Gem編集済み

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# 100.times do |index|
+#   Customer.create(family_name: "鈴木", given_name: "太郎", email: "customer_#{index}@sparta.com")
+# end
+Faker::Config.locale = :ja
+
+100.times do |index|
+  Customer.create(
+    family_name: Faker::Name.last_name,
+    given_name: Faker::Name.first_name,
+    email: Faker::Internet.email
+  )
+end
