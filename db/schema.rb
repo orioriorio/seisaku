eee65addc645bb339a4dc8664fc7ff95c9360bac
orihara <orihara.hosei@gmail.com>
2018-05-05 12:39:38 +0900

    2fin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180505024322) do
+ActiveRecord::Schema.define(version: 20180505032706) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20180505024322) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "family_name"
+    t.string "given_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
